4fd95585af451f97f9c5a9d4ced68f7e517acd76
Mattheus-co <info@mattheus.co>
2026-04-26 20:57:04 +0200

    Create subscribe.js

---
+++ api/subscribe.js
@@ -0,0 +1,50 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ message: 'Email is required' });
+  }
+
+  try {
+    const response = await fetch('https://a.klaviyo.com/api/profile-subscription-bulk-create-jobs/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'revision': '2023-10-15',
+        'Authorization': 'Klaviyo-API-Key JOUW_PRIVATE_API_KEY'
+      },
+      body: JSON.stringify({
+        data: {
+          type: 'profile-subscription-bulk-create-job',
+          attributes: {
+            list_id: 'WdFqJp',
+            profiles: {
+              data: [
+                {
+                  type: 'profile',
+                  attributes: {
+                    email: email
+                  }
+                }
+              ]
+            }
+          }
+        }
+      })
+    });
+
+    if (!response.ok) {
+      const text = await response.text();
+      return res.status(500).json({ message: text });
+    }
+
+    return res.status(200).json({ message: 'Subscribed!' });
+
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
